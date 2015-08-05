d8e8cce6d071005d8a7feeccc40a99f26217d05f
Travis Johnston & Clinton Herrick <student@epicodus.com>
2015-08-05 11:26:59 -0700

    create intergration test to take user entry and return scrabble score

---
+++ spec/scrabble_score_intergration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('scrabble score'), {:type => :feature} do
+  it ('take user entry and return scrabble score') do
+    visit('/')
+    fill_in('scrabble_word', :with => 'scrabble')
+    click_button('Submit')
+    expect(page).to have_content('14')
+  end
+end
